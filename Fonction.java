9c73f251a8069f807186dd0466e32d3dbaa1c524
Tralamy <tralamy442@gmail.com>
2020-05-09 23:35:03 -0400

    Remove Main String on Fonction.java

--- Fonction.java
+++ Fonction.java
@@ -1,11 +1,6 @@
 package net.object.test;
 
 public class Fonction {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	public static void pause(int i)  {
 		try {Thread.sleep(i);} catch(Exception e) {System.err.println("Error: " + e);}
